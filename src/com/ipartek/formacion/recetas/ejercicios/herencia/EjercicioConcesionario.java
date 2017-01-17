43a5a3b2183b2750bb5a01e3ee4bbd2de4adde41
anderuraga <ander.uraga@gmail.com>
2017-01-17 12:49:44 +0100

    package para Herencia

---
+++ src/com/ipartek/formacion/recetas/ejercicios/herencia/EjercicioConcesionario.java
@@ -0,0 +1,19 @@
+package com.ipartek.formacion.recetas.ejercicios.herencia;
+
+/**
+ * Listar todos los Vehiculos del concesionario
+ * 
+ * @author ur00
+ *
+ */
+public class EjercicioConcesionario {
+
+	public static void main(String[] args) {
+
+		// crear vehiculos
+
+		// mostrar por pantalla
+
+	}
+
+}
